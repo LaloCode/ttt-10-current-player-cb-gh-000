2504e079ecbf14e6f43dc54d5c87fd0efaa0460c
José Lerma <lalo_pichu22@hotmail.com>
2018-09-06 18:14:22 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |players|
-    players == 'X' || players == 'O' ? counter += 1 :
+    players == 'X' || players == 'O' ? counter += 1 : nil
   end
 return counter
 end
